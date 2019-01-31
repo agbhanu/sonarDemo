e233091f3a4fa014c6f01ba1351ea2838d4cb289
bhanu_agrawal <bhanu_agrawal@persistent.com>
2019-01-31 14:21:28 +0530

    initial commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+
+    public static void main(String args[]){
+        System.out.println(new HelloWorld().sumOfTwoNum(2,4));
+    }
+
+    public int sumOfTwoNum(int a,int b){
+        return a+b;
+    }
+}
---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class HelloWorldTest {
+
+    @Test
+    public void testSumOfTwoNum(){
+        assertTrue(6==new HelloWorld().sumOfTwoNum(2,4));
+    }
+}
